d5cf1dd84c042da81cf13d029af7946bdb63b4f4
AlvinOt <thisisalvinhere@gmail.com>
2023-08-28 19:46:39 +0000

    organize file to connect to db

---
+++ app/config/database_config_template.py
@@ -0,0 +1,6 @@
+db_config = {
+    'host': 'localhost',
+    'user': 'your_username',
+    'password': 'your_password',
+    'database': 'your_database_name'
+}
--- app/db_connector.py
+++ app/db_connector.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import mysql.connector
-from config import db_config
+from config.database_config import db_config
 
 # Create a connection
 connection = mysql.connector.connect(**db_config)
@@ -9,7 +9,6 @@ connection = mysql.connector.connect(**db_config)
 # Create a cursor to execute SQL queries
 cursor = connection.cursor()
 
-# Close the connection when done
+# Close the cursor and connection when done
 cursor.close()
 connection.close()
-
